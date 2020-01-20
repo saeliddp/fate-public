3329497c3bfd44133bc2847fbdee3acfd96a4941
Coco Li <kel28@uw.edu>
2020-01-20 14:48:41 -0800

    editted feedback and leaderboard

--- webapp2/settings.py
+++ webapp2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1xlxrvc)_w0hd!qv3^ip)ax#yn4@4g6o@dtp+y6=rao%^f6^tl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['shielded-tundra-32718.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['shielded-tundra-32718.herokuapp.com','localhost', '127.0.0.1']
 
 
 # Application definition
