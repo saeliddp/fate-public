491e707acbf107e52e363b185720b46878e4b6a4
Danny <saelid2517@gmail.com>
2020-03-09 16:57:37 -0700

    added city method

--- ipdata.py
+++ ipdata.py
@@ -13,4 +13,12 @@ def country_frequency():
             cfdict[details.country] = 0
         cfdict[details.country] += 1
     return cfdict
-    
+
+def city_frequency():
+    cfdict = {}
+    for r in rpds:
+        details = handler.getDetails(r.ip_addr)
+        if details.city not in cfdict:
+            cfdict[details.city] = 0
+        cfdict[details.city] += 1
+    return cfdict
